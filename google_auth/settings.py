b158d467c5f922f7db22bd3d81a98dd7fc517ed6
Markmang <markudeagha@gmail.com>
2025-12-22 14:18:24 +0100

    Add Fly.io host settings

--- google_auth/settings.py
+++ google_auth/settings.py
@@ -31,14 +31,14 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "True"
 
-RENDER_HOST = "google-auth-31e7.onrender.com"
-
-EXTRA_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
-
-ALLOWED_HOSTS = [RENDER_HOST] + EXTRA_HOSTS
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    ".fly.dev",
+]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://google-auth-31e7.onrender.com"
+    "https://*.fly.dev",
 ]
 
 
